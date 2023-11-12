a065d4399b4562bacc3c03b72a17b7458a6e7031
aeMyst <panpetertran123@gmail.com>
2023-11-11 18:32:48 -0700

    deleted some useless files, and changes naming

--- aboutus.py
+++ aboutus.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def aboutus():
     return render_template('aboutus.html')
 
 if __name__ == '__main__':
