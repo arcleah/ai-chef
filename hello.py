f571bf2054d3f860665105e3a0601b5feccd20af
arcleah <arcciepasc@gmail.com>
2023-11-12 01:05:37 -0700

    LOGIN

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ from flask import Flask, request, render_template, redirect, url_for, session, f
 import sqlite3
 app = Flask(__name__)
 app.secret_key = 'corn'  # for session management
+correct_email = 'johndoe123@gmail.com'
+correct_password = '1234'
 
 database = r"C:\Users\yo-s-\Documents\GitHub\ai-chef\sqlite\database.db"  # Define the database path here
 
@@ -43,23 +45,18 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    # error = None  # Define an error variable to hold the error message
-    # if request.method == 'POST':
-    #     email = request.form['email']
-    #     password = request.form['password']
-        
-    #     conn = create_connection(database)
-    #     auth_success = login_user(conn, email, password)
-        
-    #     if auth_success:
-    #         session['logged_in'] = True
-    #         session['user_id'] = email  # Or the actual user ID from the database
-    #         return redirect(url_for('pantry'))
-    #     else:
-    #         error = "Invalid credentials"  # Set the error message
-    
-    # # If it's a GET request or credentials are wrong, show the login form again
-    # return render_template("login.html", error=error)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == correct_email and password == correct_password:
+            # Correct credentials, redirect to pantry page
+            return redirect(url_for('pantry'))
+        else:
+            # Incorrect credentials, you may want to display an error message
+            flash('Invalid username or password', 'error')
+            return render_template("login.html")
+
     return render_template("login.html")
 
 @app.route("/signup")
