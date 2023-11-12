4b9cbf53698bcbbdd3a9543935867bb8b92a6f72
anthonytolentino <tolentno.anthony@gmail.com>
2023-11-12 07:02:25 -0700

    fixed prompt to focus the recipe on the expiring ingredients

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = 'corn'  # for session management
 correct_email = 'johndoe123@gmail.com'
 correct_password = '1234'
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="api")
 
 database = r"C:\Users\yo-s-\Documents\GitHub\ai-chef\sqlite\database.db"  # Define the database path here
 
@@ -76,10 +76,15 @@ def generate_recipe():
     items = data['itemsData']
     
     # Construct the prompt with items details
-    prompt = "Here are the items in the pantry:\n"
+    prompt = "You are a professional chef, skilled in creating recipes. I only have limited items in the pantry, some that might be expiring soon such as:\n"
     for item in items:
         prompt += f"Produce: {item['produce']}, Quantity: {item['quantity']}, Expiry Date: {item['expiryDate']}\n"
-    prompt += "Generate a recipe using these ingredients."
+    prompt += "Can you give me a detailed recipe I can make with these ingredients? Please ensure that you are focusing on the expiring ingredients from my pantry \n"
+    prompt += "(i.e. 1 day until expiration) to lower food waste before creating the recipe. Please also make sure to list the times needed for each step, \n" 
+    prompt += "the measurements (as an example, sear the steak for 2 mins on each side, boil the potatoes for 10 mins, etc), \n" 
+    prompt += "the estimated time it will take to cook the meal (i.e. overall time is 30 mins, 1 hour, etc). \n"
+    prompt += "please format it with the name of the recipe attached with the estimated time to prepare, followed by the list of ingredients and the amount needed, followed by the steps and make them detailed by including the amount of ingredients and time needed for each step"
+
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
