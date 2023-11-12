b05df8abec4df84843e50c11496530f41df9d901
anthonytolentino <tolentno.anthony@gmail.com>
2023-11-12 02:22:01 -0700

    trying to get the api to work

--- openai-test.py
+++ openai-test.py
@@ -3,15 +3,16 @@
 # to run the program, enter: python openai-test.py
 # to exit the program, press ctrl + c
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from openai import OpenAI
+
 
 OpenAI.api_key = ('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')
 client = OpenAI(api_key="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")
 
 @app.route('/submit-data', methods=['POST'])
 def submit_data():
-    items_data = request.form.get('itemsData')
+    items_data = request.json['itemsData']
 
     prompt = f"You are a professional chef, skilled in creating recipes. I want you to make 3 at a time. I have limited ingredients and I want to make a delicious meal. In my pantry, I only have {items_data}. Can you give me options on various detailed recipes I can make with these ingredients?
               Please ensure to make use of listing the times needed for each step, the measurements (as an example, 
@@ -28,7 +29,7 @@ def submit_data():
     )
 
     recipe = response.choices[0].text.strip()
-    return render_template("aboutus.html", recipe=recipe)  # Pass the recipe to the template
+    return jsonify({'recipe': recipe})
 
 if __name__ == '__main__':
     app.run(debug=True)
