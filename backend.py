5ca345cbb65068b0a61a3d9b0d67ba453c55ca2e
anthonytolentino <tolentno.anthony@gmail.com>
2023-11-12 05:48:59 -0700

    WORKING I THINK
    to run:
    make sure you have flask installed (if not: pip install Flask)
    make sure you have openAI installed (if not: pip install --upgrade openai)
    
    afterwards, run: py ./backend.py
    should give you an IP you can navigate through with a browser

--- backend.py
+++ backend.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, render_template, redirect, url_for, session, flash
-#from database import create_connection, login_user, create_user  # Importing the functions
+from flask import Flask, request, render_template, redirect, url_for, session, flash, jsonify
 import sqlite3
+from openai import OpenAI
+import json
+
 app = Flask(__name__)
 app.secret_key = 'corn'  # for session management
 correct_email = 'johndoe123@gmail.com'
 correct_password = '1234'
+client = OpenAI(api_key="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")
 
 database = r"C:\Users\yo-s-\Documents\GitHub\ai-chef\sqlite\database.db"  # Define the database path here
 
@@ -66,6 +69,31 @@ def signup():
 @app.route("/pantry")
 def pantry():
     return render_template("pantry.html")
+
+@app.route('/generate-recipe', methods=['POST'])
+def generate_recipe():
+    data = request.json
+    items = data['itemsData']
+    
+    # Construct the prompt with items details
+    prompt = "Here are the items in the pantry:\n"
+    for item in items:
+        prompt += f"Produce: {item['produce']}, Quantity: {item['quantity']}, Expiry Date: {item['expiryDate']}\n"
+    prompt += "Generate a recipe using these ingredients."
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt}
+        ]
+    )
+
+    # Extract the recipe content from the response
+    recipe_content = response.choices[0].message.content
+
+    # Return the recipe content in JSON format
+    return jsonify({'recipe': recipe_content})
+
 
 if __name__ == "__main__":
     app.run()
--- openai-test.py
+++ openai-test.py
@@ -6,30 +6,14 @@
 from flask import Flask, request, render_template, jsonify
 from openai import OpenAI
 
+OpenAI.api_key = ('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')
+client = OpenAI(api_key="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")
 
-OpenAI.api_key = ('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')
-client = OpenAI(api_key="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")
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a professional chef, skilled in creating recipes. I want you to make 3 at a time: 1 focused on my preferences, and 2 which can stray a bit from it. I have limited ingredients and I want to make a delicious meal. In my pantry, I only have [2 carrots and 2 tomatoes]. Can you give me options on various detailed recipes I can make with these ingredients? Please ensure to make use of listing the times needed for each step, the measurements (as an example, sear the steak for 2 mins on each side, boil the potatoes for 10 mins, etc), the estimated time it will take to cook the meal (i.e. overall time is 30 mins, 1 hour, etc). please format it with the name of the recipe attached with the estimated time to prepare, followed by the list of ingredients and the amount needed, followed by the steps and make them detailed by including the amount of ingredients and time needed for each step."}
+  ]
+)
 
-@app.route('/submit-data', methods=['POST'])
-def submit_data():
-    items_data = request.json['itemsData']
-
-    prompt = f"You are a professional chef, skilled in creating recipes. I want you to make 3 at a time. I have limited ingredients and I want to make a delicious meal. In my pantry, I only have {items_data}. Can you give me options on various detailed recipes I can make with these ingredients?
-              Please ensure to make use of listing the times needed for each step, the measurements (as an example, 
-              'sear the steak for 2 mins on each side', 'boil the potatoes for 10 mins', etc), the estimated time 
-              it will take to cook the meal (i.e. 'overall time is 30 mins', 1 hour, etc). Please format it with the 
-              name of the recipe attached with the estimated time to prepare, followed by the list of ingredients and 
-              the amount needed, followed by the steps and make them detailed by including the amount of ingredients 
-              and time needed for each step"
-
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo",  # Replace with the model you want to use
-        prompt=prompt,
-        max_tokens=150
-    )
-
-    recipe = response.choices[0].text.strip()
-    return jsonify({'recipe': recipe})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print(completion.choices[0].message)
