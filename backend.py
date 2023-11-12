19b81d957f27d97d36f0e39a421e729c34dcf8bd
anthonytolentino <tolentno.anthony@gmail.com>
2023-11-12 09:03:14 -0700

    fixing sign up logic

--- backend.py
+++ backend.py
@@ -14,28 +14,7 @@ database = r"C:\Users\yo-s-\Documents\GitHub\ai-chef\sqlite\database.db"  # Defi
 @app.route("/", methods = ['GET', "POST"])
 def test():
     if request.method == 'POST':
-        name = request.form.get('name')
-        dietary_restrictions = request.form.get('dietary-restrictions')
-        allergies = request.form.get('allergies')
-        preferences = request.form.get('food-preferences')
-
-        curr = sqlite3.connect(r"C:\Users\yo-s-\Documents\GitHub\ai-chef\sqlite\database.db")
-        cursor = curr.cursor()
-
-        query = "INSERT INTO users(name, dietary_restrictions, allergies, preferences) VALUES(?,?,?,?)"
-        cursor.execute(query, (name, dietary_restrictions, allergies, preferences))
-
-        results = cursor.fetchall()
-
-        for i in results:
-            print(i)
-
-        curr.commit()
-
-        cursor.close()
-        curr.close()
-
-        return "hello" + name + ""
+        return render_template("mainpage.html")
     return render_template("mainpage.html")
 
 @app.route("/aboutus")
